2e8fcf775639761c1702cdecee7b85b933e12a42
Mohammadreza Hasanrezaeian <git@mds.ir>
2025-12-17 11:20:03 +0330

    Core: added custom version string support.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -11,7 +11,8 @@
 
 #define nginx_version      1029004
 #define NGINX_VERSION      "1.29.4"
-#define NGINX_VER          "nginx/" NGINX_VERSION
+#define NGINX_VER_CUSTOM   " special version SOMARK supported by mdre3a"
+#define NGINX_VER          "nginx/" NGINX_VERSION NGINX_VER_CUSTOM
 
 #ifdef NGX_BUILD
 #define NGINX_VER_BUILD    NGINX_VER " (" NGX_BUILD ")"
